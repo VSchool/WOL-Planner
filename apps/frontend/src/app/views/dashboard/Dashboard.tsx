35b68e594f68b0de1a40f77f11b973f461ea2b23
Adrian-Walker <awalker3365@gmail.com>
2024-01-04 18:36:39 -0500

    needs new css

--- apps/frontend/src/app/views/dashboard/Dashboard.tsx
+++ apps/frontend/src/app/views/dashboard/Dashboard.tsx
@@ -1,47 +1,52 @@
 import React from "react"
+import { AuthLayout } from '../../components/authlayout/AuthLayout';
 import './dashboard.css'
-import bSmith from '../../images/dash/b_smith.svg'
-import balance from '../../images/dash/balance_btn.svg'
-import cashFlow from '../../images/dash/cashflow_btn.svg'
 import hamburger_menu from '../../images/dash/hamburger_menu.svg'
-import netWorth from '../../images/dash/net_worth.svg'
 import profile from '../../images/dash/profile.svg'
-import text from '../../images/dash/text.svg'
 import userBtn from '../../images/dash/user_btn.svg'
+import hankHill from '../../images/dash/Hank_Hill.webp'
 
 export const Dashboard = () => {
-//testing
+    //testing
 
     return (
-        <div className="page">
-
+        <AuthLayout className="h-screen">
+            <div className="page">
             <button className="burger">
-            <img src={hamburger_menu} alt="" className="hamburger_menu_dash" />
+                <img src={hamburger_menu} alt="" className="hamburger_menu_dash" />
             </button>
+
             <div className="profile_dash">
-            <img src={profile} alt="" className="profile_dash" />
+                <img src={hankHill} alt="" className="profile_dash" />
             </div>
-            <div className="bsmith">
-            <img src={bSmith} alt="" className="bsmith" id="bsmith" />
+            <div>
+                <h1 className="greeting-text">Hello, `</h1>
             </div>
-            <div className="text">
-            <img src={text} alt="" className="text" />
+
+            <div className="numbers-font">
+                <p >-275.87</p>
             </div>
-            <div className="networth">
-            <img src={netWorth} alt="" className="networth" />
+
+            <div className="page-font">
+                <p>Net Worth</p>
             </div>
+
+
             <button className="balance">
-            <img src={balance} alt="" className="balance" />
+                <span className="btn-font">Cash Flow</span>
             </button>
 
 
-                <button className="balance">
-            <img src={cashFlow} alt="" className="cashFlow" />
-                </button>
+            <button className="balance">
+                <span className="btn-font">Balance</span>
+            </button>
 
-                <button className="userBtn">
-            <img src={userBtn} alt="" className="userBtn" />
+            <div className="userBtn">
+                <button >
+
                 </button>
-                </div>
+            </div>
+            </div>
+        </AuthLayout>
     )
 }
