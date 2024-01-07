068184e7076506db9409bcea27fe6a70b1e32cf8
Adrian-Walker <awalker3365@gmail.com>
2024-01-07 17:41:54 -0500

    cleaned up css. still working

--- apps/frontend/src/app/views/dashboard/Dashboard.tsx
+++ apps/frontend/src/app/views/dashboard/Dashboard.tsx
@@ -1,49 +1,65 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import { AuthLayout } from '../../components/authlayout/AuthLayout';
 import './dashboard.css'
 import hamburger_menu from '../../images/dash/hamburger_menu.svg'
 import hankHill from '../../images/dash/Hank_Hill.webp'
+import image103 from '../../images/logos/loginGroup103.svg'
 
 export const Dashboard = () => {
-  //With Testing
+    //With Testing
 
     return (
-        <AuthLayout className="h-screen">
+        <AuthLayout>
             <div className="page">
-            <button className="burger">
-                <img src={hamburger_menu} alt="" className="hamburger_menu_dash" />
-            </button>
+                <div className="image-103">
+                    <img src={image103} alt=""  />
+                </div>
 
-            <div className="profile_dash">
-                <img src={hankHill} alt="" className="profile_dash" />
-            </div>
-            <div>
-                <h1 className="greeting-text">Hello, `</h1>
-            </div>
-
-            <div className="numbers-font">
-                <p >-275.87</p>
-            </div>
-
-            <div className="page-font">
-                <p>Net Worth</p>
-            </div>
-
-
-            <button className="balance">
-                <span className="btn-font">Cash Flow</span>
-            </button>
-
-
-            <button className="balance">
-                <span className="btn-font">Balance</span>
-            </button>
-
-            <div className="userBtn">
-                <button >
-
+                <button className="burger">
+                    <img src={hamburger_menu} alt="" className="hamburger_menu_dash" />
                 </button>
-            </div>
+
+                <div className="profile-dash-div">
+                    <img src={hankHill} alt="" className="profile_dash"/>
+                </div>
+                <div>
+                    <h1 className="greeting-text">Hello, `</h1>
+                </div>
+
+                <div className="numbers-font">
+                    <p >-275.87</p>
+                </div>
+
+                <div className="page-font">
+                    <p>Net Worth</p>
+                </div>
+
+
+                <Link to="/">
+                    <div className="cash-flow">
+                        <button >
+                            <span className="btn-font">
+                                Cash Flow
+                            </span>
+                        </button>
+                    </div>
+                </Link>
+
+                <Link to='/'>
+                    <div className="balance">
+                        <button>
+                            <span className="btn-font">Balance</span>
+                        </button>
+                    </div>
+                </Link>
+
+
+                <div className="userBtn">
+                    <button >
+
+                    </button>
+                </div>
             </div>
         </AuthLayout>
     )
