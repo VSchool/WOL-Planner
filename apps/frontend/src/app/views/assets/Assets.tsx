f6b9d27e5b957bd7cfa10caf13e3e2b4b18abd5a
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-02-15 21:54:56 -0800

    created assets directory and component w/boilerplate code

---
+++ apps/frontend/src/app/views/assets/Assets.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import { AuthLayout } from '../../components/authlayout/AuthLayout';
+import hamburger_menu from '../../images/dash/hamburger_menu.svg';
+import hankHill from '../../images/dash/Hank_Hill.webp';
+import image103 from '../../images/logos/loginGroup103.svg';
+import HamburgerNav from '../../components/hamburger-nav/HamburgerNav';
+import './assets.css';
+
+export const Assets = () => {
+  return (
+    <div>
+      <h1>Assets go here</h1>
+    </div>
+  );
+};
