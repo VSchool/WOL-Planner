8c94d38990ea7abf714c17aa50ddd3b7d1f3b88e
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-02-29 21:01:28 -0800

    re-added assets routes to router and hamburger menu

--- apps/frontend/src/app/views/router/router.tsx
+++ apps/frontend/src/app/views/router/router.tsx
@@ -11,40 +11,26 @@ import AdminPage from '../admin/AdminPage';
 import { ForgotPassword } from '../forgotPassword/ForgotPassword';
 import {SignUp} from "../signup/SignUp"
 import { Dashboard } from '../dashboard/Dashboard';
-<<<<<<< HEAD
-import { NamePage } from "../namePage/NamePage"
 import { Assets } from "../assets/Assets"
-import { AssetsInput } from "../assets/AssetsInput"
-import { UpdateAssets } from '../assets/UpdateAssets';
-import { AssetProvider } from '../assets/AssetContext';
-=======
->>>>>>> 9327c17895675227fbe9e728d3772649e9b6d559
+import { AssetsInput } from '../assets/AssetsInput';
 
 export default function Router() {
 
     return (
-      <AssetProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path='/blogs' element={<BlogsPage/>} />
-        <Route path='/blogs/blog/:id' element={<IndividualBlogPage />} />
-        <Route path='/account' element={<AccountPage/>} />
-        <Route path='/admin' element={<AdminPage/>} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/blogs/blog/:id" element={<IndividualBlogPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
-        <Route path="/namepage" element={<NamePage />}/>
         <Route path="/assets" element={<Assets />} />
         <Route path="/assets/input" element={<AssetsInput />} />
-        <Route path="/assets/update" element={<UpdateAssets />} />
-=======
->>>>>>> 9327c17895675227fbe9e728d3772649e9b6d559
       </Routes>
-
-      </AssetProvider>
     );
 }
